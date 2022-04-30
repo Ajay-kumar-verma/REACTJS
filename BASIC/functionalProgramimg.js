7c7a25b2c7a08af5c1dc1096a098ab0243acbefb
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-04-30 21:20:11 +0530

    files added

---
+++ BASIC/functionalProgramimg.js
@@ -0,0 +1,31 @@
+// let nme="AJay kumar ";
+
+// const greeting="hi "+nme;
+
+// console.log(greeting);
+
+
+const greeting=(nme)=>{
+console.log("Hi" +nme);
+}
+
+greeting("Ajay kumar verma");
+
+// --------------------------------
+// pure function should have return value and argrument 
+// it doesnt use use global variable 
+
+
+
+// function can be treated object themself 
+// In javascript everything is an Oject 
+//in javascript function is treated as object   
+
+// javascript is all about functiona and object
+
+
+// do not iterate 
+// use map , reduce ,filter ,some ,every ,fill etc...
+
+// we should go immutable object 
+
