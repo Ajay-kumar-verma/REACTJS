fb70ffb7b2252036c4229099b7dd6c69c9167e6a
Ajay kumar verma <ajay997#54@gmail.com>
2022-08-08 22:38:19 +0530

    done

---
+++ JS/Hoisting.js
@@ -0,0 +1,26 @@
+//  Hoisting declaration move at top level of their scope :globally or inside function  
+
+// Variable Hoisting  var allow hoisting but let and const does not allow hoisting 
+
+// https://www.programiz.com/javascript/hoisting
+
+
+// a = 5;
+// console.log(a);
+// var a; // 5
+
+
+
+// // program to display value
+// console.log(a); // undefined 
+// var a = 5;
+
+
+// var a;
+// console.log(a); // a is declared but not there 
+// a = 5;
+
+
+a= 5;
+console.log(a);
+let a;  // it is like cant acces before initilised
--- REACT JS/src/App.js
+++ REACT JS/src/App.js
@@ -16,9 +16,66 @@
 // import Ckbx from './Chakra/Form/Ckbx';
 // import Fmcnt from './Chakra/Form/Fmcnt';
 // import Input from './Chakra/Form/Input.js';
- import {Rspsv} from './Chakra/Layout/Rspsv.js';
-
+//  import {Rspsv} from './Chakra/Layout/Rspsv.js';
+import {useState, useEffect} from 'react';
+ 
 function App() {
+
+  let [firstSignal,setfirstSignal] =  useState("red");
+  let [seconSignal,setSecongSignal] =  useState("green");
+  let [time,settime]=useState(15); 
+  
+
+
+
+
+useEffect(() => {
+  let bool=true;
+  let x=  setInterval(()=>{
+    settime(time--); 
+   
+    
+  if(time==-2){
+      
+    if(bool){
+      setfirstSignal("green");
+      setSecongSignal("red");
+      settime(15);
+      bool=false;
+    }
+    else{ 
+      setfirstSignal("red");
+      setSecongSignal("green");
+      settime(15);
+      bool=true;  
+    }
+
+      }    
+  
+  if(time==9){
+     setfirstSignal("yellow");
+     setSecongSignal("yellow");
+  }
+  
+  if(time==-1){
+     setfirstSignal("green");
+     setSecongSignal("red");
+  }
+
+
+  
+  },1000)
+  
+return (()=>{
+  clearInterval(x);
+})
+
+
+
+})
+
+
+
   return (
    <>
    {/* <Form  /> */}
@@ -39,9 +96,41 @@ function App() {
   {/* <Fmcnt  /> */}
   {/* <Input /> */}
 
-<Rspsv  />
+{/* <Rspsv  /> */}
    <br/>
    
+
+
+
+
+
+
+
+
+
+
+
+  <p>{time}</p>
+  <p>{firstSignal}</p>
+     <br/>
+   <p>{time}</p>
+  <p>{seconSignal}</p>
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    </>
    
     );
